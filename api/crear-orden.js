034a1faad1d9586679ed6ce396c57d9e2b6f2966
infoanitamiro <infoanitamiro@gmail.com>
2026-05-30 18:11:01 -0300

    Update crear-orden.js

--- api/crear-orden.js
+++ api/crear-orden.js
@@ -26,13 +26,17 @@ const token = 'd5ce6768f10971742950595cdb2cfd1a89a7f499';
         'Content-Type': 'application/json',
         'User-Agent': 'AnitaMiroCeramics (infoanitamiro@gmail.com)'
       },
-    body: JSON.stringify({
+   body: JSON.stringify({
   contact_email: 'infoanitamiro@gmail.com',
+  contact_name: 'Cliente',
+  contact_lastname: 'Simulador',
   products: [{
     variant_id: variantId,
     quantity: cantidad,
     price: precio
   }],
+  note: `Palabra: ${palabra} | Tamaño: ${tamano} | Colores: ${colores}${notas ? ' | Notas: ' + notas : ''}`
+})
   note: `Palabra: ${palabra} | Tamaño: ${tamano} | Colores: ${colores}${notas ? ' | Notas: ' + notas : ''}`
 })
     });
